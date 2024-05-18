0e0f159bdd6e942cef114364c7cd0c35821e142b
AngeLund <lundmark.angelica@gmail.com>
2024-05-18 10:12:51 +0200

    More of rest

--- backend/index.js
+++ backend/index.js
@@ -52,6 +52,95 @@ app.get('/courses', (req, res) => {
     });
 })
 
+app.get('/students/:id', (req, res) => {
+    const {id} = req.params;
+    var mysqlConnector = mysql.createConnection({
+        host     : 'localhost',
+        user     : 'root',
+        password : '',
+        database : 'gritacademy'
+    });
+    mysqlConnector.connect((e) => {
+        if(e) {
+            return res.json({message: "error, can't connect to database"})
+        }
+
+        const query = `SELECT * from students where id=${id}`;
+        mysqlConnector.query(query, (err, result) => {
+            if(err){
+                return res.json({message: "error at query"})
+            }
+            res.json(result); 
+        })
+    });
+})
+//'/all-students-by-last-name/lname'
+app.get('/studentsFname/:fname', (req, res) => {
+    const {fname} = req.params;
+    var mysqlConnector = mysql.createConnection({
+        host     : 'localhost',
+        user     : 'root',
+        password : '',
+        database : 'gritacademy'
+    });
+    mysqlConnector.connect((e) => {
+        if(e) {
+            return res.json({message: "error, can't connect to database"})
+        }
+
+        const query = `SELECT * from students where fName="${fname}"`;
+        mysqlConnector.query(query, (err, result) => {
+            if(err){
+                return res.json({message: "error at query",err:err})
+            }
+            res.json(result); 
+        })
+    });
+})
+app.get('/studentsLname/:lname', (req, res) => {
+    const {lname} = req.params;
+    var mysqlConnector = mysql.createConnection({
+        host     : 'localhost',
+        user     : 'root',
+        password : '',
+        database : 'gritacademy'
+    });
+    mysqlConnector.connect((e) => {
+        if(e) {
+            return res.json({message: "error, can't connect to database"})
+        }
+
+        const query = `SELECT * from students where lName="${lname}"`;
+        mysqlConnector.query(query, (err, result) => {
+            if(err){
+                return res.json({message: "error at query"})
+            }
+            res.json(result); 
+        })
+    });
+})
+app.get('/studentsCourses/:id', (req, res) => {
+    const {id} = req.params;
+    var mysqlConnector = mysql.createConnection({
+        host     : 'localhost',
+        user     : 'root',
+        password : '',
+        database : 'gritacademy'
+    });
+    mysqlConnector.connect((e) => {
+        if(e) {
+            return res.json({message: "error, can't connect to database"})
+        }
+
+        const query = `SELECT * from students where id=${id}`;
+        mysqlConnector.query(query, (err, result) => {
+            if(err){
+                return res.json({message: "error at query"})
+            }
+            res.json(result); 
+        })
+    });
+})
 
 
 app.listen(port, () => {
