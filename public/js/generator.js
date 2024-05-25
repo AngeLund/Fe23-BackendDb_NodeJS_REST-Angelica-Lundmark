8f3e584929c3facc69013eef1e1169292afbf7e4
AngeLund <lundmark.angelica@gmail.com>
2024-05-25 18:28:37 +0200

    Change to Table

--- public/js/generator.js
+++ public/js/generator.js
@@ -1,6 +1,7 @@
-import {studentTemplate} from './htmlTemplate.js';
-import { courseTemplate } from './htmlTemplate.js';
-export const generateStudentList = (students) => {
+import {courseTableTemplate, studentTemplate,
+    studentTableHeader, courseTableHeader, studentTableTemplate} from './htmlTemplate.js';
+
+/*export const generateStudentList = (students) => {
     const ulElement = document.createElement('ul');
     students.forEach(student => {
         const liElement = document.createElement('li');
@@ -10,9 +11,9 @@ export const generateStudentList = (students) => {
 
     document.querySelector('.generate-list-students').innerHTML='';
     document.querySelector('.generate-list-students').appendChild(ulElement);
-}
+}*/
 
-export const generateCourseList = (course) => {
+/* export const generateCourseList = (course) => {
     const ulElement = document.createElement('ul');
     course.forEach(course => {
         const liElement = document.createElement('li');
@@ -22,5 +23,41 @@ export const generateCourseList = (course) => {
 
     document.querySelector('.generate-list-courses').innerHTML = '';
     document.querySelector('.generate-list-courses').appendChild(ulElement);
+} */
+
+
+export const generateCourseList = (course) => {
+    const tbl = document.createElement("table");
+    const tbBody = document.createElement('tbody');
+    const trHeaderElement = document.createElement('tr');
+    trHeaderElement.innerHTML = courseTableHeader();
+    tbBody.appendChild(trHeaderElement)
+    course.forEach(course => {
+        const trElement = document.createElement('tr')
+        trElement.innerHTML = courseTableTemplate(course);
+        tbBody.appendChild(trElement)
+    })
+    tbl.appendChild(tbBody)
+    document.querySelector('.generate-list-courses').innerHTML = '';
+    document.querySelector('.generate-list-courses').appendChild(tbl);
+
 }
 
+
+
+export const generateStudentList = (students) => {
+    const tbl = document.createElement("table");
+    const tbBody = document.createElement('tbody');
+    const trHeaderElement = document.createElement('tr');
+    trHeaderElement.innerHTML = studentTableHeader();
+    tbBody.appendChild(trHeaderElement)
+    students.forEach(student => {
+        const trElement = document.createElement('tr')
+        trElement.innerHTML = studentTableTemplate(student);
+        tbBody.appendChild(trElement)
+
+    });
+    tbl.appendChild(tbBody)
+    document.querySelector('.generate-list-students').innerHTML='';
+    document.querySelector('.generate-list-students').appendChild(tbl);
+}
--- public/js/htmlTemplate.js
+++ public/js/htmlTemplate.js
@@ -8,3 +8,39 @@ export const courseTemplate = (course) => {
     <h3>${course.name}</h3>
     `
 }
+
+export const courseTableHeader = () => {
+    return `
+
+        <th>Name</th>
+        <th>Description</th>
+
+    `
+}
+
+export const courseTableTemplate = (course) => {
+    return `
+
+            <td>${course.name}</td>
+            <td>${course.description}</td>
+
+    `
+}
+
+export const studentTableTemplate = (student) => {
+    return `
+
+            <td>${student.fName}</td>
+            <td>${student.lName}</td>
+            <td>${student.town}</td>
+    `
+}
+
+export const studentTableHeader = () => {
+    return `
+
+        <th>Name</th>
+        <th>Last name</th>
+        <th>From</th>
+    `
+}
